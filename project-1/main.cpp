09a722a5a5de5092e6bcadcd24e36b87b4dabff5
Sam Shafieha <sshafieha0@ivc.edu>
2022-10-28 14:41:51 -0700

    team class created

--- project-1/main.cpp
+++ project-1/main.cpp
@@ -1,6 +1,7 @@
 #include "dialog.h"
 
 #include <QApplication>
+#include <fstream>
 
 int main(int argc, char *argv[])
 {
---
+++ project-1/team.h
@@ -0,0 +1,22 @@
+#ifndef TEAM_H
+#define TEAM_H
+
+#include <QMainWindow>
+#include <QObject>
+#include <QWidget>
+#include <string>
+
+using namespace std;
+
+class team
+{
+public:
+    team();
+
+private:
+    string name;
+    string stadium;
+
+};
+
+#endif // TEAM_H
