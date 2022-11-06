a2aa35a996c3d7436f9d92d4ce2472255638afcf
Sam Shafieha <sshafieha0@ivc.edu>
2022-11-05 19:12:28 -0700

    FINALLY MANAGED TO DESPLAY THE EXCEL.CSV FILE

--- project-1/desplayteam.cpp
+++ project-1/desplayteam.cpp
@@ -16,9 +16,28 @@ desplayTeam::~desplayTeam()
 
 void desplayTeam::on_pushButton_2_clicked() //display teams (unsorted)
 {
-    QFile file("C:/Users/samsh/Desktop/pdfs/Fall 2022/CS 1C/Qt Projects/project-1-football-pamphlet/project-1/NFL Information.csv");
+    QString path = "C:/Users/samsh/Desktop/pdfs/Fall 2022/CS 1C/Qt Projects/project-1-football-pamphlet/project-1/NFL Information.csv";
+    QFile file(path);
     file.open(QIODevice::ReadOnly);
-    QTextStream stream(&file);
-    QString toQString = stream.readAll();
-    ui->textBrowser->setText(toQString);
+    QStringList hlabels;
+
+    QStringList wordList;
+    QTextStream inFile(&file);
+    int row = 0;
+    QString readLine = inFile.readLine();
+    QStringList listValue = readLine.split(',');
+    ui->tableWidget->setColumnCount(listValue.size());
+    ui->tableWidget->setHorizontalHeaderLabels(listValue);
+
+    while (!inFile.atEnd()){
+        QString readLine = inFile.readLine();
+        QStringList listValue = readLine.split(',');
+
+        row++;
+        ui->tableWidget->setRowCount(row);
+        for (int k = 0; k < listValue.size(); k++){
+            ui->tableWidget->setItem(row-1, k, new QTableWidgetItem(listValue[k]));
+        }
+    }
+
 }
