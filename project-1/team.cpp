9d62f8a13fac782e7f2bab4f0062afd82329d9c9
larasabha <115130219+larasabha@users.noreply.github.com>
2022-11-08 12:39:36 -0800

    Set methods (#10)
    * set methods in h file
    
    * set methods in cpp file

--- project-1/team.cpp
+++ project-1/team.cpp
@@ -4,15 +4,57 @@ team::team()
 {
 
 }
-
 QString team::getName(){
     return name;
 }
-
 void team::setName(QString name){
     this->name = name;
 }
-
+QString getStaduim(){
+    return stadium;
+}
 void team::setStaduim(QString stadium){
     this->stadium = stadium;
+}
+QString getSeating(){
+    return seatingCapacity;
+}
+int setSeating(int seatingCapacity){
+    this->seatingCapacity = seatingCapacity;
+}
+QString getLocation(){
+    return location;
+}
+void setLocation(QString location){
+    this->location = location;
+}
+QString getConference(){
+    return conference;
+}
+void setConference(QString conference){
+    this->conference = conference;
+}
+QString getDivision(){
+    return division;
+}
+void setDivision(QString division){
+    this->division = division;
+}
+QString getSurface(){
+    return surface;
+}
+void setSurface(QString surface){
+    this->surface = surface;
+}
+QString getRoof(){
+    return roof;
+}
+void setRoof(QString roof){
+    this->roof = roof;
+}
+QString getOpenDate(){
+    return openDate;
+}
+void setOpenDate(QString openDate){
+    this->openDate = openDate;
 }
